55c8145663c35e5688a410b384aec62e6f7a0276
Sorriso <luizfelipejr@outlook.com>
2016-01-31 14:29:47 -0200

    config bourbon, neat and refills and create CSS workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,35 @@
 var gulp = require('gulp'),
+  browserSync = require('browser-sync'),
   autoprefixer = require('gulp-autoprefixer'),
   sass = require('gulp-sass'),
-  bourbon = require("node-bourbon").includePaths,
-  rename = require("gulp-rename"),
-  minifyCss = require('gulp-minify-css'),
+  bourbon = require('node-bourbon').includePaths,
+  neat = require("node-neat").includePaths,
+  cssnano = require('gulp-cssnano'),
   sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', () => {
-  return gulp.
-  src('./assets/sass/*.scss').
-  pipe(sourcemaps.init()).
-  pipe(sass({
-    includePaths: ["styles"].concat(bourbon)
-  })).
-  pipe(autoprefixer('last 4 versions')).
-  pipe(gulp.dest('./public/css')).
-  pipe(sourcemaps.write('maps')).
-  pipe(minifyCss({processImport: false})).
-  pipe(rename({
-    suffix: '.min'
-  })).
-  pipe(gulp.dest('./public/css'));
-});
+  // Live reload
+  gulp.task('watch', ['browserSync', 'sass'], function() {
+  	gulp.watch('./assets/sass/*.scss', ['sass']);
+  	gulp.watch('./public/*.html').on('change', browserSync.reload);
+  });
 
-gulp.task('watch', function() {
-  gulp.watch(['assets/sass/**/*.scss'], ['sass']);
-});
+  // Compile SASS
+  gulp.task('sass', () => {
+    return gulp.
+    src('./assets/sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: bourbon,
+      includePaths: neat
+    }))
+    .pipe(autoprefixer('last 4 versions'))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+  });
 
-gulp.task('default', ['sass']);
+  // Compiles all tasks
+  gulp.task('default', ['sass']);
